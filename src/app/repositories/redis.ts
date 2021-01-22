564dde73f0fa3ebf37f95cb46cd28b3f5ef559b5
igortomaz-bit <igor.tomaz@estudante.uffs.edu.br>
2021-01-21 21:45:14 -0300

    Correção redis startar desativado localmente

--- src/app/repositories/redis.ts
+++ src/app/repositories/redis.ts
@@ -5,18 +5,26 @@ class Redis {
   private host: string;
   private port: number;
   private client: RedisClient;
+  private enabled: boolean;
 
   constructor() {
     this.host = process.env.REDIS_HOST;
     this.port = Number(process.env.REDIS_PORT);
+    this.enabled = (process.env.REDIS_ENABLE === 'true');
     this.createClient();
   }
 
   private createClient() {
+    if (!this.enabled)
+      return;
+
     this.client = redis.createClient({host: this.host, port: this.port});
   }
 
   public async getFromCache(key: string, complement: string) {
+    if (!this.enabled)
+      return;
+
     const getAsync = promisify(this.client.get).bind(this.client);
 
     return getAsync(`${key}${complement}`)
@@ -32,6 +40,9 @@ class Redis {
 } 
 
 public async saveInCache(key: string, complement: string, content: string) {
+  if (!this.enabled)
+      return;
+
   const setAsync = promisify(this.client.set).bind(this.client);
 
   setAsync(`${key}${complement}`, content, 'EX', Number(process.env.REDIS_EXPIRATION))
