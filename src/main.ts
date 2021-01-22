0903dbd8a679f760864f5c1559e01a7afb8c7714
igortomaz-bit <igor.tomaz@estudante.uffs.edu.br>
2021-01-21 21:01:43 -0300

    Adição docker compose + envs redis

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8081;
 app.server.listen(PORT, () => { console.log(`Server running on port ${PORT}.`)});
